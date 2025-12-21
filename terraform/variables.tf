56a3d1d9c055da79e0c7ca2280374bdb94fc1156
ShalevNatan <141844381+ShalevNatan@users.noreply.github.com>
2025-12-21 13:34:44 +0200

    Add new variables for master and worker resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
-variable "master_name" { default = "k8s-master" }
+# Root variables
+variable "master_cpu" { default = 4 }
+variable "master_memory" { default = 8192 }
 variable "worker_count" { default = 3 }
-variable "worker_name_prefix" { default = "k8s-worker" }
+variable "worker_cpu" { default = 2 }
+variable "worker_memory" { default = 8192 }
+variable "host_only_network" { default = "Host-Only" }
+variable "master_ip" { default = "192.168.100.10" }
+variable "worker_ip_start" { default = 11 } # last octet for first worker
